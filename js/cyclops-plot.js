2f9a8103c58c2d5c7140e5f8acbb3cc1f4248065
ryan spangler <ryan.spangler@gmail.com>
2013-02-12 21:39:34 -0800

    generating fit functions for arbitrary sets of data

---
+++ js/cyclops-plot.js
@@ -0,0 +1,53 @@
+cyclops.plotFit = function(processing) {
+  // display the fit process somehow
+
+  var data = [0.5, 0.55, 0.7, 0.71, 0.44, 0.42, 0.14, 0.24, 0.46, 0.66];
+  var fit = cyclops.generateFit(data, 6);
+
+  function fitData(fit, dx) {
+    // transform a fitted function into an array of data
+
+    var data = [];
+    var index = 0;
+    for (x = 0.0; x <= 1.0; x += dx) {
+      var y = fit(x);
+      data[index] = y;
+      index++;
+    }
+    return data;
+  }
+
+  function drawData(data) {
+    // draw an arbitrary array of data
+
+    var dx = processing.width / (data.length - 1);
+    var xprev = 0.0;
+    var yprev = data[0];
+    for (d = 1; d < data.length; d++) {
+      var x = xprev + dx;
+      var y = data[d];
+      processing.line(xprev, (1 - yprev) * processing.height, 
+                      x, (1 - y) * processing.height);
+      xprev = x;
+      yprev = y;
+    }
+  }
+
+  processing.setup = function() {
+    processing.size(800, 600);
+  }
+
+  processing.draw = function() {
+    var snapshot = fitData(fit, 0.01);
+    processing.background(Math.random() * 100);
+    processing.stroke(0, 0, 250);
+    drawData(data);
+    processing.stroke(250, 0, 0);
+    drawData(snapshot);
+  };
+}
+
+window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  var processingInstance = new Processing(canvas, cyclops.plotFit);
+};
---
+++ js/cyclops.js
@@ -0,0 +1,104 @@
+var cyclops = function() {
+  // given a stream of data, generate a polynomial fit to that stream
+  // that can be called in its place
+
+  var inverse = function(v) {
+    // take the inverse of each component of a vector
+
+    var inv = [];
+    var len = v.length;
+    for (var o = 0; o < len; o++) {
+      inv[o] = (v[o] === 0) ? 0 : 1.0 / v[o];
+    }
+    return inv;
+  };
+
+  var buildMatrix = function(data, degree) {
+    // given an array of data, build a matrix from that data where
+    // each row is a data point and each column is an ascending integral power
+    // of that data point.
+
+    var size = data.length;
+    if (size > 1) {
+      var xStep = 1.0 / (size - 1);
+      var M = [];
+      for (var j = 0; j < size; j++) {
+        var row = [];
+        for (var k = 0; k < degree; k++) {
+          var x = xStep * j;
+          row[k] = Math.pow(x, k);
+        }
+        M[j] = row;
+      }
+
+      return M;
+    } else {
+      return data;
+    }
+  }
+
+  var pseudoInverse = function(M) {
+    // given a matrix, perform a singular value decomposition and use
+    // the result to do a pseudoinverse on that matrix
+
+    // perform the singular value decomposition
+    var singular = numeric.svd(M);
+    var U = singular.U;
+    var V = singular.V;
+    var S = singular.S;
+
+    // find the inverse of the singular values
+    var Sinverse = inverse(S);
+
+    // construct a new inverse transpose matrix from the inverted singular values
+    var dim = numeric.dim(M);
+    var Stranspose = numeric.rep([dim[1], dim[0]], 0);
+    for (var z = 0; z < S.length; z++) {
+      Stranspose[z][z] = Sinverse[z];
+    }
+
+    // perform the transformations in reverse and transposed!
+    var VS = numeric.dot(V, Stranspose);
+    var Minverse = numeric.dot(VS, numeric.transpose(U));
+
+    // return the newly constructued pseudoinverse
+    return Minverse;
+  }
+
+  var polyfit = function(data, degree) {
+    // given an array of data and the maximum polynomial degree to seek a fit for,
+    // find the coefficients for the fitted polynomial
+
+    var size = data.length;
+    if (size > 0) {
+      var M = buildMatrix(data, degree);
+      var Minverse = pseudoInverse(M);
+      var result = numeric.dot(Minverse, data);
+      return result;
+    } else {
+      return [];
+    }
+  };
+
+  var generateFit = function(data, degree) {
+    // given an array of data and the maximal degree to seek a fit for,
+    // construct a function that emulates the given data over the range [0..1]
+
+    var poly = polyfit(data, degree);
+    return function(t) {
+      var sum = 0;
+      for (var w = 0; w < poly.length; w++) {
+        sum += poly[w] * Math.pow(t, w);
+      }
+      return sum;
+    }
+  };
+
+  return {
+    inverse: inverse,
+    buildMatrix: buildMatrix,
+    pseudoInverse: pseudoInverse,
+    polyfit: polyfit,
+    generateFit: generateFit
+  };
+} ();
