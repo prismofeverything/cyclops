006066e1ed68aae3d54ae0d9ae30286b82fac914
ryan spangler <ryan.spangler@gmail.com>
2013-05-30 13:41:01 -0700

    motion based on keyframe data

---
+++ js/motion.js
@@ -0,0 +1,46 @@
+function extractFrameValues(framevalues) {
+  var xs = [];
+  var ys = [];
+
+  for (var i = 0; i < framevalues.values.length; i++) {
+    var framevalue = framevalues.values[i];
+    xs.push(framevalue.time);
+    ys.push(framevalue.value[0]);
+  }
+
+  return [xs, ys];
+}
+
+var frames, fit;
+var start, now, phase;
+var period;
+var width, height;
+var radius;
+
+function cyclopsMotion(processing) {
+  processing.setup = function() {
+    width = 1200;
+    height = 600;
+    processing.size(width, height);
+    period = 2000;
+    radius = 40;
+    start = new Date();
+    frames = extractFrameValues(framevalues);
+    fit = cyclops.generateCubic(normalizeData(frames[0]), normalizeData(frames[1]));
+  }
+
+  processing.draw = function() {
+    now = new Date();
+    phase = ((now - start) % period) / period;
+    curve = fit(phase);
+    processing.background(0, 0, 0);
+    processing.stroke(250, 250, 250);
+    processing.fill(125, 0, 250);
+    processing.arc(curve * width, height * 0.5, radius, radius, 0, 2 * Math.PI);
+  }
+}
+
+window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  var processingInstance = new Processing(canvas, cyclopsMotion);
+};
