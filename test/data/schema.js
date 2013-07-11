721bef84526808da219a77b6b4c36605a524c1b0
Ben Purdy <ben.purdy@gmail.com>
2013-07-10 17:51:37 -0700

    Adding new test files to illustrate some bugs

--- test/data/schema.js
+++ test/data/schema.js
@@ -1,90 +1,55 @@
-
 // Keyframe data format:
 
 var myTweenData = {
-
-	"position" : {
-		"duration" : 1.2,
-		"startTime" : 0,
-		"keys" : [
-			{
-				"time" : 0.0,
-				"value" : [9,5,1],
-				"in" : {
-					"type" : "bezier",
-					"influence" : 30,
-					"speed" : 9.3
+	"layerName" : {
+		"position" : {
+			"duration" : 1.2,
+			"startTime" : 0,
+			"keys" : [
+				{
+					"time" : 0.0,
+					"value" : [9, 5, 1],
+					"in" : {
+						"type" : "bezier",
+						"influence" : 30,
+						"speed" : 9.3
+					},
+					"out" : {
+						"type" : "bezier",
+						"influence" : 40,
+						"speed" : 9.3
+					}
 				},
-				"out" : {
-					"type" : "bezier",
-					"influence" : 40,
-					"speed" : 9.3
-				}
-			},
-			{
-				"time" : 0.2,
-				"value" : [3,2,2],
-				"in" : {
-					"type" : "bezier",
-					"influence" : 80,
-					"speed" : 9.3
+				{
+					"time" : 0.2,
+					"value" : [3,2,2],
+					"in" : {
+						"type" : "bezier",
+						"influence" : 80,
+						"speed" : 9.3
+					},
+					"out" : {
+						"type" : "bezier",
+						"influence" : 60,
+						"speed" : 9.3
+					}
 				},
-				"out" : {
-					"type" : "bezier",
-					"influence" : 60,
-					"speed" : 9.3
+				{
+					"time" : 1.0,
+					"value" : [5,10,5],
+					"in" : {
+						"type" : "bezier",
+						"influence" : 70,
+						"speed" : 9.3
+					},
+					"out" : {
+						"type" : "bezier",
+						"influence" : 30,
+						"speed" : 9.3
+					}
 				}
-			},
-			{
-				"time" : 1.0,
-				"value" : [5,10,5],
-				"in" : {
-					"type" : "bezier",
-					"influence" : 70,
-					"speed" : 9.3
-				},
-				"out" : {
-					"type" : "bezier",
-					"influence" : 30,
-					"speed" : 9.3
-				}
-			}
-		]
-	},
-
-	"opacity" : {
-		"duration" : 1.2,
-		"startTime" : 0.25,
-		"keys" : [
-			{
-				"time" : 0.0,
-				"value" : [9],
-				"in" : {
-					"type" : "bezier",
-					"influence" : 10.0,
-					"speed" : 9.3
-				},
-				"out" : {
-					"type" : "bezier",
-					"influence" : 60.0,
-					"speed" : 9.3
-				}
-			},
-			{
-				"time" : 1.0,
-				"value" : [3],
-				"in" : {
-					"type" : "bezier",
-					"influence" : 30.0,
-					"speed" : 9.3
-				},
-				"out" : {
-					"type" : "bezier",
-					"influence" : 90.0,
-					"speed" : 9.3
-				}
-			}
-		]
+			]
+		}
 	}
 };
 
@@ -92,4 +57,4 @@ var myTweenData = {
 // Using the api:
 
 // var foo = cyclops.loadTween(myTweenData);
-// foo.position.func(0.3);
+// foo["layerName"].position.func(0.3);
---
+++ test/js/cyclops-css.js
@@ -0,0 +1,108 @@
+var previousTime    = 0;
+var tweenTime       = 0;
+
+var tweenDuration   = keyframeData.FunFun.position.duration;
+var curve           = cyclops.loadCurve(keyframeData.FunFun);
+
+function update(){
+
+  var now = new Date().getTime();
+
+  tweenTime += (now - previousTime) * 0.001;
+  previousTime = now;
+
+  // loop
+  if(tweenTime >= tweenDuration){
+    tweenTime = 0;
+  }
+
+  var t = (document.getElementById("animate").checked) ? (tweenTime / tweenDuration) : Number(document.getElementById("time_value").value);
+  
+  requestAnimationFrame(update);
+
+  updateSourcePreview(t);
+  updateCyclopsPreview(t);
+  document.getElementById("time").innerHTML = t;
+}
+
+
+function updateCyclopsPreview(t) {
+  var position    = curve.position.func(t);
+  var scale       = curve.scale.func(t)[0] * 0.5 + 0.5;
+  var rotation    = curve.rotation.func(t)[0] * 360;
+
+  var x = rawFrameData["position"].min[0] + position[0] * (rawFrameData["position"].max[0] - rawFrameData["position"].min[0]);
+  var y = rawFrameData["position"].min[1] + position[1] * (rawFrameData["position"].max[1] - rawFrameData["position"].min[1]);
+
+  document.getElementById("cyclopsSquare").style.webkitTransform = "translate(" + x + "px, " + y + "px) rotate(" + rotation + "deg) scale(" + scale + ", " + scale + ")";
+}
+
+
+function updateSourcePreview(t) {
+  var seconds = t * tweenDuration;
+  var idx = Math.floor(rawFrameData["position"].data.length * t);
+
+  var scale       = rawFrameData["scale"].data[idx].val[0] / 100;
+  var rotation    = rawFrameData["rotation"].data[idx].val[0];
+
+  var x = rawFrameData["position"].data[idx].val[0];
+  var y = rawFrameData["position"].data[idx].val[1];
+
+  document.getElementById("sourceSquare").style.webkitTransform = "translate(" + x + "px, " + y + "px) rotate(" + rotation + "deg) scale(" + scale + ", " + scale + ")";
+}
+
+
+window.onload = function() {
+  update();
+
+  drawGraph(curve["position"].func, "position", 0);
+
+  document.getElementById("property").onchange = function(){
+    var property = document.getElementById("property").value;
+    var parts = property.split("|");
+    drawGraph( curve[parts[0]].func, parts[0], Number(parts[1]));
+  }
+};
+
+
+function padDigits(number, digits) {
+    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+}
+
+
+function drawGraph(func, propertyName, valueIndex) {
+  var canvas = document.getElementById("graph");
+  var ctx = document.getElementById("graph").getContext("2d");
+
+  var yScale = canvas.height / 2;
+  var yOffset = canvas.height / 4;
+
+  ctx.fillStyle = "#c0c0c0";
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+
+  ctx.fillStyle = "#f0f0f0";
+  ctx.fillRect(0,yOffset, canvas.width, yScale);
+
+  ctx.beginPath();
+  ctx.moveTo(0,func(0)[valueIndex] * yScale + yOffset);
+  for(var i = 0; i < rawFrameData[propertyName].data.length; i++){
+
+    var frm = rawFrameData[propertyName].data[i];
+    var t = frm["t"] / tweenDuration;
+    ctx.lineTo(t * canvas.width, func(t)[valueIndex] * yScale + yOffset);
+  }
+  ctx.strokeStyle = 'green';
+  ctx.stroke();
+
+
+  ctx.beginPath();
+  ctx.moveTo(0, func(0)[valueIndex] * yScale + yOffset);
+  for(var i = 0; i < rawFrameData[propertyName].data.length; i++){
+    var frm = rawFrameData[propertyName].data[i];
+    var t = frm["t"] / tweenDuration;
+    var rawVal = (frm["val"][valueIndex] - rawFrameData[propertyName].min[valueIndex]) / (rawFrameData[propertyName].max[valueIndex] - rawFrameData[propertyName].min[valueIndex]);
+    ctx.lineTo(t * canvas.width, rawVal * yScale + yOffset);
+  }
+  ctx.strokeStyle = 'red';
+  ctx.stroke();
+}
